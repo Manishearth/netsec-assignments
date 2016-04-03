68c3f2ca6b1dd30c901824aebad5dc8b18b35597
Manish Goregaokar <manishsmail@gmail.com>
2016-04-03 07:24:58 +0530

    Reveal values

--- js.js
+++ js.js
@@ -9,32 +9,97 @@ var permutedSolution = newEmptyArray();
 var nonces = newEmptyArray();
 var hashes = newEmptyArray();
 
+var state = "idle";
+
+/// http://stackoverflow.com/a/10142256/1198729
+Array.prototype.shuffle = function() {
+  var i = this.length, j, temp;
+  if ( i == 0 ) return this;
+  while ( --i ) {
+     j = Math.floor( Math.random() * ( i + 1 ) );
+     temp = this[i];
+     this[i] = this[j];
+     this[j] = temp;
+  }
+  return this;
+}
+
 function newEmptyArray() {
 	ret = new Array(9);
 	for(i=0;i<ret.length;i++) {
-		ret[i] = new Array(9);
+		ret[i] = ["","","","","","","","",""];
 	}
 	return ret;
 }
-function genTable(name) {
+function genTable(name, disabled) {
+	disattr="";
+	if(disabled) {
+		disattr = "disabled";
+	}
 	document.write("<table class=sudoku>");
 			for (let i =0; i<9; i++) {
 			document.write("<tr>");
 			for (let j=0; j<9;j++) {
-				document.write("<td><input size='1%' id='"+name+"-"+i+""+j+"'/></td>")
+				document.write("<td><input size='1%' "+disattr+" id='"+name+"-"+i+""+j+"'/></td>")
 			}
 			document.write("</tr>");
 		}
 	document.write("</table>")
 }
 
+function updateState(newState) {
+	state = newState;
+	for(button of ["permute", "nonce", "hash", "send", "verify", "start"]) {
+		document.getElementById(button).disabled = true;
+	}
+
+	switch (state) {
+		case "started":
+			document.getElementById("permute").disabled=false;
+			break;
+		case "permuted":
+			document.getElementById("nonce").disabled=false;
+			foreach("in-nonce", (g,i,j) => nonces[i][j] = g.value="");
+			foreach("in-sha", (g,i,j) => hashes[i][j] = g.value="");
+			break;
+		case "nonced":
+			document.getElementById("hash").disabled=false;
+			foreach("in-sha", (g,i,j) => hashes[i][j] = g.value="");
+			break;
+		case "hashed":
+			document.getElementById("send").disabled=false;
+			break;
+		case "sent":
+			document.getElementById("reveal").disabled=false;
+			break;
+		case "revealed":
+			document.getElementById("verify").disabled=false;
+			break;
+		case "verified":
+			document.getElementById("permute").disabled=false;
+			break;
+	}
+}
+
 function foreach(name, f) {
 	for(let i=0;i<dim;i++) {
 		for(let j=0; j<dim; j++) {
-			f(document.getElementById(name+'-'+i+""+j),i,j);
+			f(access(name,i,j),i,j);
 		}
 	}
 }
+
+function foreachA(arr, f) {
+	for(let i=0;i<dim;i++) {
+		for(let j=0; j<dim; j++) {
+			f(arr[i][j],i,j);
+		}
+	}
+}
+function access(name, i, j) {
+	return document.getElementById(name+'-'+i+""+j);
+}
+
 function reset() {
 	for(grid of ["preset","reveal-val", "reveal-nonce", "reveal-sha", "in-val", "in-nonce", "in-sha"]) {
 		foreach(grid, (g) => g.value="");
@@ -43,6 +108,8 @@ function reset() {
 	permutedSolution = newEmptyArray();
 	nonces = newEmptyArray();
 	hashes = newEmptyArray();
+
+	updateState("idle");
 }
 
 function prefill() {
@@ -57,10 +124,13 @@ function prefill() {
 		}
 		preset[i][j] = g.value;
 	});
+
+	updateState("started");
 }
 
 
 function init(){
+	updateState("started");
 
 }
 
@@ -72,76 +142,50 @@ function hash(x, y){
 
 function generateRandomNonces(){
 	foreach("in-nonce",(g, i, j)=> nonces[i][j] = g.value = Math.random().toString(32).substring(7));
+	updateState("nonced");
 }
 
 function generateHashes(){
 	foreach("in-sha", (g,i,j) => {
 		hashes[i][j] = g.value = hash(nonces[i][j], permutedSolution[i][j])
 	})
+	updateState("hashed");
 }
 
 function permute(){
-	for(let i=0;i<dim;++i){
-		for(let j=0;j<dim;++j){
-			d[i][j] = '0';
-			document.getElementById('d' + i + j).value = d[i][j];
-		}
-	}
+	let a = [1,2,3,4,5,6,7,8,9];
+	a.shuffle();
+	foreach("in-val", (g, i, j) => permutedSolution[i][j] = g.value = a[g.value - 1])
+	updateState("permuted");
 }
 
-function showHashes(){
-	for(let i=0;i<dim;++i){
-		for(let j=0;j<dim;++j){
-			c[i][j] = f[i][j];
-			document.getElementById("c"+i+j).value = c[i][j];
-		}
-	}
-}
-
-function clear(){
-	init();
-	revealed = false;
-	hashGenerated = false;
-	for(let i=0;i<dim;++i){
-		for(let j=0;j<dim;++j){
-			if(preset[i][j] != '')
-				d[i][j] = preset[i][j];
-			else d[i][j] = '';
-
-			document.getElementById('a'+i+j).value = '';
-			document.getElementById('b'+i+j).value = '';
-			document.getElementById('c'+i+j).value = '';
-			document.getElementById('d'+i+j).value = d[i][j];
-			document.getElementById('e'+i+j).value = '';
-			document.getElementById('f'+i+j).value = '';
-		}
-	}
+function send() {
+	foreach("reveal-sha", (g,i,j) =>g.value=hashes[i][j])
+	updateState("sent");
 }
 
 function reveal(){
-	if(revealed){
-		clear()
-		revealed = false;
-	}
-	var x = document.getElementById('g0').value;
-	var y = parseInt(document.getElementById('g1').value);
-
-	if(x == '0'){
+	var type = document.getElementById('type').value;
+	var segment = parseInt(document.getElementById('segment').value);
+	let revealedPresets = new Set();
+	if(type == '0'){
 		for(let j=0;j<dim;++j){
-			document.getElementById('a'+y+j).value = d[y][j];
-			document.getElementById('b'+y+j).value = e[y][j];
-			a[y][j] = d[y][j];
-			b[y][j] = e[y][j];
-			revealpos[y][j] = true;
+			let i = segment;
+			access('reveal-val',i, j).value = permutedSolution[i][j];
+			access('reveal-nonce',i, j).value = nonces[i][j];
+			if(preset[i][j] != "") {
+				revealedPresets.add(preset[i][j]);
+			}
 		}
 	}
-	else if(x == '1'){
+	else if(type == '1'){
 		for(let i=0;i<dim;++i){
-			document.getElementById('a'+i+y).value = d[i][y];
-			document.getElementById('b'+i+y).value = e[i][y];
-			a[i][y] = d[i][y];
-			b[i][y] = e[i][y];
-			revealpos[i][y] = true;
+			let j = segment;
+			access('reveal-val',i, j).value = permutedSolution[i][j];
+			access('reveal-nonce',i, j).value = nonces[i][j];
+			if(preset[i][j] != "") {
+				revealedPresets.add(preset[i][j]);
+			}
 		}
 	}
 	else{
@@ -149,25 +193,22 @@ function reveal(){
 		let q = (y%sdim)*sdim;
 		for(let i=p;i<p+sdim;++i){
 			for(let j=q;j<q+sdim;++j){
-				document.getElementById('a'+i+j).value = d[i][j];
-				document.getElementById('b'+i+j).value = e[i][j];
-				a[i][j] = d[i][j];
-				b[i][j] = e[i][j];
-				revealpos[i][j] = true;
+				let j = segment;
+				access('reveal-val',i, j).value = permutedSolution[i][j];
+				access('reveal-nonce',i, j).value = nonces[i][j];
+				if(preset[i][j] != "") {
+					revealedPresets.add(preset[i][j]);
+				}
 			}
 		}
 	}
-
-	for(let i=0;i<dim;++i){
-		for(let j=0;j<dim;++j){
-			if(preset[i][j] != ''){
-				document.getElementById('a'+i+j).value = d[i][j];
-				document.getElementById('b'+i+j).value = e[i][j];
-				revealpos[i][j] = true;
-			}
+	console.log(revealedPresets);
+	foreachA(preset, (g, i, j) => {
+		if(revealedPresets.has(g)) {
+			access('reveal-val',i, j).value = permutedSolution[i][j];
+			access('reveal-nonce',i, j).value = nonces[i][j];
 		}
-	}
-	revealed = true;
+	})
 }
 
 function error(){
